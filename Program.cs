08b0f41bd5d17a59ee530a06ab585922bfca8242
Vineet Singh <vineet.usingh31@gmail.com>
2026-09-13 00:39:40 +0530

    update for swagger in post

--- Program.cs
+++ Program.cs
@@ -83,11 +83,11 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAngularDev");
